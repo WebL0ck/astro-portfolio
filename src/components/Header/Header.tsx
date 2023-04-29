99bd0be2ed86dcdbdec2f9b8073b53c7c0628912
WebLock <antontamazlukar@gmail.com>
2023-04-29 22:40:26 +0300

    CHANGED: header markup & styling

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -18,13 +18,15 @@ function Header() {
   return (
     <header
       className={classNames({
-        "sticky top-0 flex h-16 w-full items-center justify-between px-6": true,
+        "sticky top-0 flex h-16 w-full items-center justify-between px-6 backdrop-blur-lg": true,
         "border-opacity-1 border-b border-solid  border-b-gray-8 transition duration-300 ease bg-blurry-1":
           scrollY > 70,
         "border-b border-solid border-b-gray-8 border-opacity-0 transition duration-300 ease": scrollY <= 70,
       })}
     >
-      <Image src="icons/logo.svg" alt="logo" width={55} height={29} />
+      <Link href={ROUTES.HOME}>
+        <Image src="icons/logo.svg" alt="logo" width={55} height={29} />
+      </Link>
       <nav className="flex h-3 items-center gap-3">
         <Link href={ROUTES.WORK} className={headerLinkStyles}>
           work
