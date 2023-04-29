490d86a258e4caf465ecae40585514d55d66e00b
WebLock <antontamazlukar@gmail.com>
2023-04-29 22:41:14 +0300

    EXTENDED: typography component

--- src/components/shared/Typography/Typography.tsx
+++ src/components/shared/Typography/Typography.tsx
@@ -1,9 +1,30 @@
 interface Props {
-  children: string;
+  children?: any;
+  className?: string | undefined;
+  element?: "span" | "p";
 }
 
-function Typography({ children, ...rest }: Props) {
-  return <p {...rest}>{children}</p>;
+function Typography({ children, className, element = "p", ...rest }: Props) {
+  const renderRootElement = () => {
+    switch (element) {
+      case "span":
+        return (
+          <span className={className} {...rest}>
+            {children}
+          </span>
+        );
+
+      case "p":
+      default:
+        return (
+          <p className={className} {...rest}>
+            {children}
+          </p>
+        );
+    }
+  };
+
+  return renderRootElement();
 }
 
 export default Typography;
