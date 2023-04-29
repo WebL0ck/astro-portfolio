676e8be3067a0c6f0889f21d5d921cde68ac5c22
WebLock <antontamazlukar@gmail.com>
2023-04-29 20:39:49 +0300

    ADDED: custom hook

---
+++ src/hooks/useWindowScrollPosition.ts
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+
+type ScrollPosition = {
+  scrollX: number;
+  scrollY: number;
+};
+
+export const useWindowScrollPosition = (): ScrollPosition => {
+  const [scrollPosition, setScrollPosition] = useState<ScrollPosition>({ scrollX: 0, scrollY: 0 });
+
+  const handleScroll = (): void => {
+    const currentScrollPosition: ScrollPosition = { scrollX: window.scrollX, scrollY: window.scrollY };
+    setScrollPosition(currentScrollPosition);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll, { passive: true });
+
+    return (): void => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return scrollPosition;
+};
