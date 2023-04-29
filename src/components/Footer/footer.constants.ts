69bdb1c1ee6ecbdf2411b0c2c18926482ab758a1
WebLock <antontamazlukar@gmail.com>
2023-04-29 22:39:56 +0300

    ADDED: footer constants

---
+++ src/components/Footer/footer.constants.ts
@@ -0,0 +1,30 @@
+export const socialIcons = [
+  {
+    name: "github",
+    icon: "/icons/socials/github.svg",
+  },
+  {
+    name: "telegram",
+    icon: "/icons/socials/telegram.svg",
+  },
+  {
+    name: "linkedIn",
+    icon: "/icons/socials/linked-in.svg",
+  },
+  {
+    name: "gmail",
+    icon: "/icons/socials/gmail.svg",
+  },
+  {
+    name: "slack",
+    icon: "/icons/socials/slack.svg",
+  },
+  {
+    name: "instagram",
+    icon: "/icons/socials/instagram.svg",
+  },
+  {
+    name: "discord",
+    icon: "/icons/socials/discord.svg",
+  },
+];
