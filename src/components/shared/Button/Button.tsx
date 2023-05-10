ef1758f4383e0694a506525a3aff8e4eb19e0315
WebLock <antontamazlukar@gmail.com>
2023-05-10 03:06:55 +0300

    EXTENEDED: button component logic

--- src/components/shared/Button/Button.tsx
+++ src/components/shared/Button/Button.tsx
@@ -1,22 +1,33 @@
 import classNames from "classnames";
 
+import Box from "../Box/Box";
+
 interface Props {
   children?: any;
   withBorder?: boolean;
   withHover?: boolean;
   withTransition?: boolean;
+  withGradient?: boolean;
   className?: string | undefined;
   onClick?: () => void;
 }
 
-function Button({ children, className, withBorder = true, withHover = true, withTransition = true, onClick }: Props) {
+function Button({
+  children,
+  withGradient,
+  className,
+  withBorder = true,
+  withHover = true,
+  withTransition = true,
+  onClick,
+}: Props) {
   console.log("🚀 ~ Button ~ withHover:", withHover);
 
-  return (
+  const renderButton = () => (
     <button
       className={classNames(
         {
-          "z-0 flex justify-center items-center rounded": true,
+          "z-0 flex justify-center items-center rounded cursor-pointer": true,
           "border border-solid border-white px-8 py-2": withBorder,
           "hover:bg-blurry-2": withHover,
           "transition duration-300 ease": withTransition,
@@ -28,6 +39,16 @@ function Button({ children, className, withBorder = true, withHover = true, with
       {children}
     </button>
   );
+
+  if (withGradient) {
+    return (
+      <Box className="rounded shadow-button hover:shadow-button-active transition duration-500 ease animate-gradient bg-gradient-to-r from-orange-gradient-1 via-orange-gradient-1 to-orange-gradient-2 p-[1px]">
+        {renderButton()}
+      </Box>
+    );
+  }
+
+  return renderButton();
 }
 
 export default Button;
