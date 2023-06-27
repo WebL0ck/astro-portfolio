94f8a86757d3f9c496a385f64993616b3ac4a04b
WebLock <antontamazlukar@gmail.com>
2023-06-27 03:25:23 +0300

    EXTENDED: works object

--- src/api/works/works.api.ts
+++ src/api/works/works.api.ts
@@ -1,10 +1,40 @@
 import { TAGS } from "../tags/tags.api";
 
-export const WORKS = [
-  {
+export const WORKS = {
+  SOCIOGRAM: {
     title: "Sociogram",
     description:
       "A blazing fast and secure wallet to track your crypto actives. Easy buy and sell crypto as well. Suck my dick bro. Sound well but with big fat cock should be more enjoyable!",
     tags: [TAGS.REACT, TAGS.REDUX, TAGS.NODEJS],
+    position: "Middle Front-end developer",
+    images: {
+      mobile: "/images/sociogram-mobile.png",
+      desktop: "/images/sociogram-desktop.png",
+    },
   },
-];
+  BATTLEPRO: {
+    title: "Battlepro",
+    description:
+      "A blazing fast and secure wallet to track your crypto actives. Easy buy and sell crypto as well. Suck my dick bro. Sound well but with big fat cock should be more enjoyable!",
+    tags: [TAGS.REACT, TAGS.REDUX, TAGS.NODEJS],
+    position: "Middle Front-end developer",
+    images: {
+      logo: "/icons/battlepro_logo.png",
+      mobile: "/images/battlepro-mobile.png",
+      desktop: "/images/battlepro-desktop.png",
+    },
+  },
+  MYEYEDR: {
+    title: "MyEyeDr",
+    description:
+      "A blazing fast and secure wallet to track your crypto actives. Easy buy and sell crypto as well. Suck my dick bro. Sound well but with big fat cock should be more enjoyable!",
+    tags: [TAGS.REACT, TAGS.REDUX, TAGS.NODEJS],
+    position: "Middle Front-end developer",
+    images: {
+      logo: "/icons/myeyedr-logo.svg",
+      mobile: "/images/myeyedr-mobile.png",
+      mobile_1: "/images/myeyedr-mobile-1.png",
+      mobile_2: "/images/myeyedr-mobile-2.png",
+    },
+  },
+};
